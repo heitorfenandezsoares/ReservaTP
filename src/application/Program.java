bdeca78e11dbba809b2c357f5f65a7dec498ebf3
heitor <120781647+heitorfenandezsoares@users.noreply.github.com>
2024-04-27 15:19:16 -0300

    começando o codigo

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import entities.InvalidDateException;
+
+public class Program {
+
+	public static void main(String[] args) {
+	  throw new InvalidDateException();
+
+	}
+
+}
---
+++ src/entities/InvalidDateException.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class InvalidDateException extends RuntimeException {
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public InvalidDateException(){
+		super("data invalida");
+		
+	}
+
+	
+	
+}
